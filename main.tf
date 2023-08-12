7d37e77413197ceb307adcabfe9a62db55e539d0
Mehdi Pasha <135350614+mehdidevops2030@users.noreply.github.com>
2023-08-12 22:32:42 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -86,16 +86,6 @@ module "eks" {
   }
 
   eks_managed_node_groups = {
-    one = {
-      name = "node-group-1"
-
-      instance_types = ["t2.micro"]
-
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
-    }
-
     two = {
       name = "node-group-2"
 
